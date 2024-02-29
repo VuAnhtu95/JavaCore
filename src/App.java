831c6eb5b89bf34be5db1cd6a4a535bcd9f2b23a
Tu <anhtu2971995@gmail.com>
2024-02-29 16:04:35 +0700

    Feat: Find Fibonacci Number

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of Fibonacci numbers you want?");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n){
+        int f0 = 0;
+        int f1 = 1;
+        int result = 1;
+
+        if (n <= 1) {
+            return n;
+        }else{
+            for (int i = 2; i < n; i++) {
+                f0 = f1;
+                f1 = result;
+                result = f0 + f1;
+            }
+            return result;
+        }
     }
 }
