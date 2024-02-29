7c971a009d31c12514dc83ed2ef5c87183fb9fe9
Tu <anhtu2971995@gmail.com>
2024-02-29 16:39:32 +0700

    Feat: Find Prime Number

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (int i = 0; i < 100; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrimeNumber(int n){
+        if (n <= 1) {
+            return false;
+        }else{
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
